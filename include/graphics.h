40bd6f823a450b0f61dda3854561ab85f92bfdda
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-07 06:45:09 +0800

    Add portable graphics compatibility shim

---
+++ include/graphics.h
@@ -0,0 +1,7 @@
+#ifndef ASTER_GRAPHICS_COMPAT_H
+#define ASTER_GRAPHICS_COMPAT_H
+#include <stdint.h>
+uint32_t gfx_text(uint32_t x,uint32_t y,const char *s,uint32_t rgb,uint32_t scale);
+uint32_t gfx_display_text(uint32_t x,uint32_t y,const char *s,uint32_t rgb);
+void gfx_line_h(uint32_t x,uint32_t y,uint32_t w,uint32_t rgb);
+#endif
