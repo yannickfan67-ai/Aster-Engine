319fd03c7f78fe5362f2f41ee8a15b52fa05d810
yannickfan67-ai <yannickfan67@gmail.com>
2026-09-09 21:29:08 +0800

    aster: add image URI platform callback

--- include/aster_platform.h
+++ include/aster_platform.h
@@ -4,11 +4,12 @@
 #include <stddef.h>
 
 #define ASTER_PLATFORM_ABI_MAJOR 1u
-#define ASTER_PLATFORM_ABI_MINOR 0u
+#define ASTER_PLATFORM_ABI_MINOR 1u
 #define ASTER_PLATFORM_ABI_VERSION ((ASTER_PLATFORM_ABI_MAJOR << 16) | ASTER_PLATFORM_ABI_MINOR)
 #define ASTER_PLATFORM_CAP_TEXT         (1ull << 0)
 #define ASTER_PLATFORM_CAP_DISPLAY_TEXT (1ull << 1)
 #define ASTER_PLATFORM_CAP_LINES        (1ull << 2)
+#define ASTER_PLATFORM_CAP_IMAGE_URI    (1ull << 3)
 
 typedef struct AsterPlatformOps {
     uint32_t abi_version;
@@ -17,7 +18,8 @@ typedef struct AsterPlatformOps {
     uint32_t (*draw_text)(void *context,uint32_t x,uint32_t y,const char *text,uint32_t rgb,uint32_t scale);
     uint32_t (*draw_display_text)(void *context,uint32_t x,uint32_t y,const char *text,uint32_t rgb);
     void (*draw_line_h)(void *context,uint32_t x,uint32_t y,uint32_t width,uint32_t rgb);
-    void *reserved[8];
+    int (*draw_image_uri)(void *context,uint32_t x,uint32_t y,uint32_t width,uint32_t height,const char *uri);
+    void *reserved[7];
 } AsterPlatformOps;
 
 int aster_platform_install(const AsterPlatformOps *ops,void *context);
